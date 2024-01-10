32d61234318d5914db3a61cc3ef29250a97ef4d9
childofukraine <sailant686@gmail.com>
2024-01-10 17:48:50 +0200

    feat: parser

--- index.js
+++ index.js
@@ -1,12 +1,96 @@
-import express from "express";
+const bodyParser = require('body-parser');
+const express = require('express');
+const puppeteer = require('puppeteer');
 
 const app = express();
+const port = 3000;
 
-app.get("/", (req, res) => {
-  return res.status(200).json({
-    status: 200,
-    message: "Hit!",
-  });
+
+app.use(bodyParser.json())
+const _headers = {
+  'user-agent':
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+  'upgrade-insecure-requests': '1',
+  accept:
+    'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+  'accept-encoding': 'gzip, deflate, br',
+  'accept-language': 'en-US,en;q=0.9,en;q=0.8',
+};
+
+
+app.post('/', async (req, res) => {
+  const url = req.body.url
+  const response = await parse(url)
+  res.json(response)
 });
 
-app.listen(8080, () => console.log("Server running port 8080")); 
+function extractBrandAndImage(schema) {
+  if (schema && Array.isArray(schema)) {
+    const firstProduct = schema[0];
+
+    if (firstProduct) {
+      const brand =
+        typeof firstProduct.brand === 'object'
+          ? firstProduct.brand.name
+          : typeof firstProduct.brand === 'string'
+          ? firstProduct.brand
+          : '';
+
+      const image = Array.isArray(firstProduct.image)
+        ? firstProduct.image[0]
+        : typeof firstProduct.image === 'string'
+        ? firstProduct.image
+        : '';
+
+      return { brand, image };
+    }
+  }
+
+  return { brand: 'N/A', image: 'N/A' };
+}
+
+async function parse(url) {
+  try {
+    const browser = await puppeteer.launch({
+      headless: 'new',
+      defaultViewport: {
+        width: 1366,
+        height: 1024,
+      },
+    });
+    const page = await browser.newPage();
+    await page.setExtraHTTPHeaders(_headers);
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
+
+    const schema = await page.evaluate(() => {
+      const ldJsonScript = Array.from(
+        document.querySelectorAll('script[type="application/ld+json"]')
+      );
+      return ldJsonScript.map((script) => JSON.parse(script.textContent));
+    });
+
+    const title = await page.title();
+    const brand = extractBrandAndImage(schema).brand;
+    const href = extractBrandAndImage(schema).image;
+    const description = await page.$eval(
+      "head > meta[name='description']",
+      (element) => element.content
+    );
+
+
+    await browser.close();
+
+    return {
+      title,
+      href,
+      brand,
+      description,
+    };
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+}
+
+app.listen(port, () => {
+  console.log("listening on port 3000")
+})
