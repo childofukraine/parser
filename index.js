b33050ebd6a2fe1762ee0b9181fd22c15b8e121d
childofukraine <sailant686@gmail.com>
2024-01-10 15:43:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    status: 200,
+    message: "Hit!",
+  });
+});
+
+app.listen(8080, () => console.log("Server running port 8080")); 
