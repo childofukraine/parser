bfa2b6bc1d73192e2664cfa11e29e793c3d5b238
childofukraine <sailant686@gmail.com>
2024-01-17 12:09:17 +0200

    feat: new version for docker

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const puppeteer = require('puppeteer');
+const bodyParser = require("body-parser");
+const express = require("express");
+const puppeteer = require("puppeteer-core");
+const cors = require("cors");
 
 const app = express();
-const port = 3000;
+const port = 8080;
 
+app.use(cors());
+app.use(bodyParser.json());
 
-app.use(bodyParser.json())
-const _headers = {
-  'user-agent':
-    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
-  'upgrade-insecure-requests': '1',
-  accept:
-    'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
-  'accept-encoding': 'gzip, deflate, br',
-  'accept-language': 'en-US,en;q=0.9,en;q=0.8',
-};
-
-
-app.post('/', async (req, res) => {
-  const url = req.body.url
-  const response = await parse(url)
-  res.json(response)
+app.post("/", async (req, res) => {
+  const url = req.body.url;
+  const response = await parse(url);
+  res.json(response);
 });
+
+app.listen(port, () => {
+  console.log("listening on port 8080 (test cors)");
+});
+
+const _headers = {
+  "user-agent":
+    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+  "upgrade-insecure-requests": "1",
+  accept:
+    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+  "accept-encoding": "gzip, deflate, br",
+  "accept-language": "en-US,en;q=0.9,en;q=0.8",
+};
 
 function extractBrandAndImage(schema) {
   if (schema && Array.isArray(schema)) {
@@ -30,29 +35,32 @@ function extractBrandAndImage(schema) {
 
     if (firstProduct) {
       const brand =
-        typeof firstProduct.brand === 'object'
+        typeof firstProduct.brand === "object"
           ? firstProduct.brand.name
-          : typeof firstProduct.brand === 'string'
+          : typeof firstProduct.brand === "string"
           ? firstProduct.brand
-          : '';
+          : "";
 
       const image = Array.isArray(firstProduct.image)
         ? firstProduct.image[0]
-        : typeof firstProduct.image === 'string'
+        : typeof firstProduct.image === "string"
         ? firstProduct.image
-        : '';
+        : "";
 
       return { brand, image };
     }
   }
 
-  return { brand: 'N/A', image: 'N/A' };
+  return { brand: "N/A", image: "N/A" };
 }
 
 async function parse(url) {
   try {
     const browser = await puppeteer.launch({
-      headless: 'new',
+      headless: "new",
+      executablePath: "/usr/bin/chromium-browser",
+      ignoreDefaultArgs: ["--disable-extensions"],
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
       defaultViewport: {
         width: 1366,
         height: 1024,
@@ -60,7 +68,7 @@ async function parse(url) {
     });
     const page = await browser.newPage();
     await page.setExtraHTTPHeaders(_headers);
-    await page.goto(url, { waitUntil: 'domcontentloaded' });
+    await page.goto(url, { waitUntil: "domcontentloaded" });
 
     const schema = await page.evaluate(() => {
       const ldJsonScript = Array.from(
@@ -77,7 +85,6 @@ async function parse(url) {
       (element) => element.content
     );
 
-
     await browser.close();
 
     return {
@@ -87,10 +94,6 @@ async function parse(url) {
       description,
     };
   } catch (error) {
-    console.error('Error:', error.message);
+    console.error("Error:", error.message);
   }
 }
-
-app.listen(port, () => {
-  console.log("listening on port 3000")
-})
