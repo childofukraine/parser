dc4c8bda8b9dd87ae3bc700fae69b54947b92470
ArturHoncharuk <73081258+ArturHoncharuk@users.noreply.github.com>
2024-02-08 12:46:26 +0200

    Update hostname.js

--- utils/hostname.js
+++ utils/hostname.js
@@ -1,7 +1,11 @@
 function $hostname(url) {
-  const parsedURL = new URL(url);
-  const siteName = parsedURL.hostname.split('.')[1];
-  return siteName?.charAt(0).toUpperCase() + siteName.slice(1);
+  const hostnameRegex = /^(?:https?:\/\/)?(?:www\.)?([^\/]+)/i;
+  const match = url.match(hostnameRegex);
+  if (match) {
+    const siteName = match[1].split('.')[0]; // Extract the first part of the hostname
+    return siteName.charAt(0).toUpperCase() + siteName.slice(1); // Capitalize the first letter
+  }
+  return '';
 }
 
 module.exports = $hostname;
