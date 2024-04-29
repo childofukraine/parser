392ad52917c4221e7dd61749270d4afb81d9a241
childofukraine <sailant686@gmail.com>
2024-04-29 17:29:21 +0300

    feat: proxy config chages

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.post('/', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port} (v0.5)`);
+  console.log(`Listening on port ${port} (v0.10)`);
 });
 
 const defaultHeaders = {
